ce6a35e445e072cc68aa008f87e7595b4852ac90
senthilmalli.parameswaran@marks-and-spencer.com <senthilmalli.parameswaran@marks-and-spencer.com>
2024-09-14 21:36:14 +0000

    Removes redundant metrics collection from pl_test_observability pipeline

--- test2/pipelines/pl_test_observability/code/pl_test_observability/pipeline.py
+++ test2/pipelines/pl_test_observability/code/pl_test_observability/pipeline.py
@@ -8,21 +8,7 @@ from pl_test_observability.graph import *
 
 def pipeline(spark: SparkSession) -> None:
     df_src_ds = src_ds(spark)
-    df_src_ds = collectMetrics(
-        spark, 
-        df_src_ds, 
-        "graph", 
-        "q9wWy3qWhN7vA3MaUPjxz$$JWHoJfhoCPtoSPHps5Srw", 
-        "Bw9PbkgUtV97-4P6UBi6u$$fB_90fnVFB60CcTBXEQBx"
-    )
     df_add_created_timestamp = add_created_timestamp(spark, df_src_ds)
-    df_add_created_timestamp = collectMetrics(
-        spark, 
-        df_add_created_timestamp, 
-        "graph", 
-        "MbhYgXsvxWX1rA0YeDjYg$$XodXDMjGRdw9yMb4XeEnm", 
-        "_-2y4z9urpuvVTT5wYrBk$$_jGTtusxYSohFs81Y1dLP"
-    )
     tgt_ds(spark, df_add_created_timestamp)
 
 def main():
@@ -33,14 +19,10 @@ def main():
                 .appName("pl_test_observability")\
                 .getOrCreate()
     Utils.initializeFromArgs(spark, parse_args())
-    MetricsCollector.initializeMetrics(spark)
-    spark.conf.set("prophecy.collect.basic.stats", "true")
-    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/pl_test_observability")
     registerUDFs(spark)
-    MetricsCollector.instrument(spark = spark, pipelineId = "pipelines/pl_test_observability", config = Config)(
-        MetricsCollector.withSparkOptimisationsDisabled(pipeline)
-    )
+    
+    MetricsCollector.instrument(spark = spark, pipelineId = "pipelines/pl_test_observability", config = Config)(pipeline)
 
 if __name__ == "__main__":
     main()
