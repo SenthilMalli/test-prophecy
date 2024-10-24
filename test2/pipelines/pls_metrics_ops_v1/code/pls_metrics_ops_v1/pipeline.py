d462e9998d55bb933371ccdf9ecb69e6e28b6378
senthilmalli.parameswaran@marks-and-spencer.com <senthilmalli.parameswaran@marks-and-spencer.com>
2024-10-24 05:23:45 +0000

    Adds new job 'test1' and enhances 'pls_metrics_ops_v1' pipeline with metrics collection

--- test2/pipelines/pls_metrics_ops_v1/code/pls_metrics_ops_v1/pipeline.py
+++ test2/pipelines/pls_metrics_ops_v1/code/pls_metrics_ops_v1/pipeline.py
@@ -8,7 +8,21 @@ from pls_metrics_ops_v1.graph import *
 
 def pipeline(spark: SparkSession) -> None:
     df_src_tbl_pq_metrics_test2 = src_tbl_pq_metrics_test2(spark)
+    df_src_tbl_pq_metrics_test2 = collectMetrics(
+        spark, 
+        df_src_tbl_pq_metrics_test2, 
+        "graph", 
+        "pgMALTu9cupCVVqPAYSBC$$eik3Ornf3SIH53FZCTbzy", 
+        "MyxEePxzeIczNclKh9zAP$$JpcVv0RdN1PAJZvz3sPfR"
+    )
     df_add_created_timestamp = add_created_timestamp(spark, df_src_tbl_pq_metrics_test2)
+    df_add_created_timestamp = collectMetrics(
+        spark, 
+        df_add_created_timestamp, 
+        "graph", 
+        "cG8Ws0TqFcwcs5l2Q88Ps$$ktrhJe00Pc0lca0AUnNf8", 
+        "oU_bDFhRIgwYAiZrH6VGc$$g05RHvM6HAdnQRoj2nN70"
+    )
     tgt_tbl_pq_metric_r1(spark, df_add_created_timestamp)
 
 def main():
@@ -19,10 +33,14 @@ def main():
                 .appName("pls_metrics_ops_v1")\
                 .getOrCreate()
     Utils.initializeFromArgs(spark, parse_args())
+    MetricsCollector.initializeMetrics(spark)
+    spark.conf.set("prophecy.collect.basic.stats", "true")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/pls_metrics_ops_v1")
     registerUDFs(spark)
-    
-    MetricsCollector.instrument(spark = spark, pipelineId = "pipelines/pls_metrics_ops_v1", config = Config)(pipeline)
+    MetricsCollector.instrument(spark = spark, pipelineId = "pipelines/pls_metrics_ops_v1", config = Config)(
+        MetricsCollector.withSparkOptimisationsDisabled(pipeline)
+    )
 
 if __name__ == "__main__":
     main()
