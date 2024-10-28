13341484afe76f5e38a8608cf9dbc4dcf9fce294
senthilmalli.parameswaran@marks-and-spencer.com <senthilmalli.parameswaran@marks-and-spencer.com>
2024-10-28 19:56:14 +0000

    Adds new pipeline pl_metrics_ops_v5 and updates tgt_tbl_insert_metric3 dataset - Changes field types and descriptions in tgt_tbl_insert_metric3 - Renames table in tgt_tbl_metric3 from metrics_test3 to metrics_insert_test3 - Implements new components and configurations for pl_metrics_ops_v5 pipeline

---
+++ test2/pipelines/pl_metrics_ops_v5/code/pl_metrics_ops_v5/graph/src_csv_metric3.py
@@ -0,0 +1,18 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.utils import *
+from prophecy.libs import typed_lit
+from pl_metrics_ops_v5.config.ConfigStore import *
+from pl_metrics_ops_v5.functions import *
+
+def src_csv_metric3(spark: SparkSession) -> DataFrame:
+    return spark.read\
+        .schema(
+          StructType([
+            StructField("trandate", StringType(), True), StructField("salesamt", StringType(), True), StructField("ymd", StringType(), True)
+        ])
+        )\
+        .option("header", True)\
+        .option("sep", ",")\
+        .csv("dbfs:/mnt/landing/prophecy/ops/transsample.csv")
--- test2/pipelines/pl_metrics_ops_v5/code/pl_metrics_ops_v5/graph/tgt_tbl_insert_metric3.py
+++ test2/pipelines/pl_metrics_ops_v5/code/pl_metrics_ops_v5/graph/tgt_tbl_insert_metric3.py
@@ -3,8 +3,8 @@ from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from prophecy.utils import *
 from prophecy.libs import typed_lit
-from pl_metrics_ops_v3.config.ConfigStore import *
-from pl_metrics_ops_v3.functions import *
+from pl_metrics_ops_v5.config.ConfigStore import *
+from pl_metrics_ops_v5.functions import *
 
-def tgt_tbl_metric3(spark: SparkSession, in0: DataFrame):
+def tgt_tbl_insert_metric3(spark: SparkSession, in0: DataFrame):
     in0.write.format("delta").mode("append").saveAsTable("`spark_catalog`.`sst_ingest_ndev`.`metrics_insert_test3`")
