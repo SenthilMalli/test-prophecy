bb0bedb8eff85cc2ee8ce733dfc5875ce23f44ce
senthilmalli.parameswaran@marks-and-spencer.com <senthilmalli.parameswaran@marks-and-spencer.com>
2024-11-05 13:59:46 +0000

    Updates field type and prophecy-libs version, adds metrics collection - Changes field type of 'ymd' in tgt_tbl_insert_metric3 dataset - Updates prophecy-libs dependency to 1.9.14 in multiple pipelines - Adds metrics collection to pl_metrics_ops_v5 pipeline

--- test2/pipelines/pl_metrics_ops_v5/code/pl_metrics_ops_v5/pipeline.py
+++ test2/pipelines/pl_metrics_ops_v5/code/pl_metrics_ops_v5/pipeline.py
@@ -8,7 +8,21 @@ from pl_metrics_ops_v5.graph import *
 
 def pipeline(spark: SparkSession) -> None:
     df_src_csv_metric3 = src_csv_metric3(spark)
+    df_src_csv_metric3 = collectMetrics(
+        spark, 
+        df_src_csv_metric3, 
+        "graph", 
+        "NsnNWwAY26Nqu20rnGxzl$$ci3dAJbAn5USgVWd2enjn", 
+        "ig0NHwxouoAW3dWl3O9CK$$6TkzD7dE9l7N0B5HqOhsV"
+    )
     df_sales_projection = sales_projection(spark, df_src_csv_metric3)
+    df_sales_projection = collectMetrics(
+        spark, 
+        df_sales_projection, 
+        "graph", 
+        "vJfhXIlV1xuXUKVlpZ-f6$$SqKvqghS5PrV8j3ZGR3nq", 
+        "aozo5q1uffa6-5EfbUnEh$$TkEyflq26yDj_qlmGmeJa"
+    )
     tgt_tbl_insert_metric3(spark, df_sales_projection)
 
 def main():
@@ -19,10 +33,14 @@ def main():
                 .appName("pl_metrics_ops_v5")\
                 .getOrCreate()
     Utils.initializeFromArgs(spark, parse_args())
+    MetricsCollector.initializeMetrics(spark)
+    spark.conf.set("prophecy.collect.basic.stats", "true")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/pl_metrics_ops_v5")
     registerUDFs(spark)
-    
-    MetricsCollector.instrument(spark = spark, pipelineId = "pipelines/pl_metrics_ops_v5", config = Config)(pipeline)
+    MetricsCollector.instrument(spark = spark, pipelineId = "pipelines/pl_metrics_ops_v5", config = Config)(
+        MetricsCollector.withSparkOptimisationsDisabled(pipeline)
+    )
 
 if __name__ == "__main__":
     main()
--- test2/pipelines/pls_metrics_ops_v1/code/setup.py
+++ test2/pipelines/pls_metrics_ops_v1/code/setup.py
@@ -7,7 +7,7 @@ setup(
     package_data = {'prophecy_config_instances' : ['*.json', '*.py', '*.conf']},
     description = 'workflow',
     install_requires = [
-'prophecy-libs==1.9.9'],
+'prophecy-libs==1.9.14'],
     entry_points = {
 'console_scripts' : [
 'main = pls_metrics_ops_v1.pipeline:main'], },
